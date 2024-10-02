ca6315978cefe3ec15ce164c85a37a6def4403b9
Manu Siddhartha <77057415+manusid123@users.noreply.github.com>
2024-10-02 21:40:34 +0530

    Add files via upload

---
+++ inputChain.py
@@ -0,0 +1,106 @@
+from typing import List, Tuple
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from PyPDF2 import PdfReader
+import os
+
+def get_pdf_text(directory) -> List[Document]:
+    """Extracts text from PDF files and creates Document objects.
+
+    Args:
+        directory: Path to the directory containing PDF files.
+
+    Returns:
+        A list of Document objects, each representing a PDF file.
+    """
+    all_docs = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".pdf"):
+            pdf_path = os.path.join(directory, filename)
+            loader = PyPDFLoader(pdf_path)
+            docs = loader.load()
+            for i, doc in enumerate(docs):
+                doc.metadata["source"] = f"{filename}_page_{i+1}" 
+            all_docs.extend(docs)
+    return all_docs
+
+
+
+def get_text_chunks(docs: List[Document]):
+    """Splits documents into smaller chunks while preserving metadata.
+
+    Args:
+        docs: A list of Document objects.
+
+    Returns:
+        A list of Document chunks.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=10000, chunk_overlap=1000
+    )
+    all_chunks = []
+    for doc in docs:
+        chunks = text_splitter.split_documents([doc])
+        for chunk in chunks:
+            chunk.metadata["source"] = doc.metadata["source"]
+        all_chunks.extend(chunks)
+    return all_chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
+    vector_store.save_local("siva_puraan_faiss_index")
+
+
+def get_conversational_chain():
+    prompt_template = """
+    Assume you are a true devotee of Lord Shiva and you love to answer queries regarding Lord Shiva.
+
+    Context:
+    {context}
+
+    Question:
+    {question}
+    Note: Dont answer if any question is not related to Lord Shiva at all. Politely decline to answer by mentioning I am true devotee of lord shiva, happy to answer query regarding lord shiva.
+    If someone greets you simply greets back with Om Namah Shivaay.
+
+    Also detect the language of user and respond in the user's language as far as possible.
+   
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.1)
+
+    prompt = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+def user_input(user_question: str, docs: List[Document]) -> dict:
+    """Gets the answer to the user's question, along with reference document sources.
+
+    Args:
+        user_question: The user's question.
+        docs: A list of Documents returned from the similarity search.
+
+    Returns:
+        A dictionary containing the model's answer and the reference document sources.
+    """
+
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True
+    )
+
+    
+
+    return response['output_text']
---
+++ main.py
@@ -0,0 +1,103 @@
+import os
+import json
+
+import streamlit as st
+from typing import List, Tuple
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+import inputChain
+from PIL import Image  # Import the Image module from Pillow
+
+
+# Access the API key
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+
+# Load the PNG icon image
+jnaneshvara_icon = Image.open("janesvaraicon.ico")
+
+GOOGLE_API_KEY=st.secrets['GOOGLE_API_KEY']
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+new_db = FAISS.load_local(
+    "siva_puraan_faiss_index", embeddings, allow_dangerous_deserialization=True
+)
+
+# streamlit page configuration
+st.set_page_config(
+    page_title="Shiva Puraan",
+    page_icon=jnaneshvara_icon,
+    layout="centered"
+)
+
+st.sidebar.image("janesvara_logo.png")
+st.sidebar.markdown(
+    """
+    <div style="background-color:#f0f0f0; padding:10px; border-radius:5px;">
+       <h3>About <b>Jnaneshvara</b> </h3>
+<p>
+Meet <b>Jnaneshvara</b>, a chatbot created to answer your questions about Lord Shiva. Drawing upon authentic and sacred sources within Hindu mythology—specifically the <i>Shiva Purana</i>, <i>Skanda Purana</i>, <i>Linga Purana</i>, and <i>Shiv Gita</i> (part of the <i>Padma Purana</i>)—<b>Jnaneshvara</b> aims to help you connect with and learn more about Lord Shiva. 
+</p>
+<br>
+<b>Disclaimer:</b> All information provided is sourced from these texts. Please forgive any inaccuracies, as <b>Jnaneshvara</b> is an AI-based bot.
+    
+    """,
+    unsafe_allow_html=True
+)
+    
+
+
+# working_dir = os.path.dirname(os.path.abspath(__file__))
+# config_data = json.load(open(f"{working_dir}/config.json"))
+
+# GROQ_API_KEY = config_data["GROQ_API_KEY"]
+
+# # save the api key to environment variable
+# os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+# client = Groq()
+
+# initialize the chat history as streamlit session state of not present already
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+
+# streamlit page title
+st.title("🔱📿🕉🪘𓆗 Har Har Mahadev 🙏")
+
+# display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message('assistant', avatar='janesvaraicon.png',):
+        st.markdown(message["content"])
+
+
+# input field for user's message:
+user_prompt = st.chat_input("Namaste 🙏! I'm Jnaneshvara. Ask me anything about Lord Shiva...")
+
+if user_prompt:
+
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+    docs = new_db.similarity_search(user_prompt)
+    # sens user's message to the LLM and get a response
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant"},
+        *st.session_state.chat_history
+    ]
+
+    response = inputChain.user_input(user_prompt, docs)
+
+    assistant_response = response
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # display the LLM's response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
+
